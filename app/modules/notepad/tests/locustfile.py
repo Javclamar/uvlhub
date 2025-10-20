a35f3d2027c3bb5c2cdf3c17e1117414093af2d6
Javclamar <javclamar@alum.us.es>
2025-10-20 12:10:16 +0200

    Makes unit testing and load testing for notepad module

--- app/modules/notepad/tests/locustfile.py
+++ app/modules/notepad/tests/locustfile.py
@@ -1,21 +1,25 @@
-from locust import HttpUser, TaskSet, task
+from locust import HttpUser, TaskSet, task, between
 from core.environment.host import get_host_for_locust_testing
 
 
 class NotepadBehavior(TaskSet):
-    def on_start(self):
-        self.index()
-
-    @task
-    def index(self):
+    
+    @task(1)
+    def load_list(self):
+        print("Cargando lista de notepads")
         response = self.client.get("/notepad")
 
         if response.status_code != 200:
             print(f"Notepad index failed: {response.status_code}")
 
+    @task(2)
+    def create_notepad(self):
+        print("Creando un nuevo notepad")
+        response = self.client.post("/notepad/create", json={"title": "Notepad locust", "body": "Body locust"})
+        if response.status_code != 200:
+            print(f"Notepad creation failed: {response.status_code}")
 
 class NotepadUser(HttpUser):
+    wait_time = between(1, 5)
     tasks = [NotepadBehavior]
-    min_wait = 5000
-    max_wait = 9000
     host = get_host_for_locust_testing()
